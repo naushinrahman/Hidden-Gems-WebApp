8de57405e05abfa8a38c41f948a12f6e6186d88b
Naushin <naushin.r@gmail.com>
2023-08-08 21:28:08 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+
+/*
+3. A “Join” Button that submits the form. When the form is submitted, it should
+POST to https://httpbin.org/post, and only allow the user to submit when a
+valid email address has been entered. NEED THIS IN JS*/
