8018dfce20d3fa0af6b35c21994a5cac8c03696b
Naushin <naushin.r@gmail.com>
2023-08-10 00:47:10 -0400

    adding js

---
+++ js/data.js
@@ -0,0 +1,10 @@
+// Attach reviewData to the global window Object
+// so other scripts can access it at runtime.
+window.reviewData = [
+    {
+        name:'Naushin',
+        date: '2023-08-09',
+        rating: '5/5',
+        comment: 'Its so peaceful and serene, i love listening to the birds',
+    }
+];
