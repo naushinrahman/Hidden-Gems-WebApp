704f8c7a62361d30d7a840496e8d0a36c859bd63
Naushin <naushin.r@gmail.com>
2023-08-10 21:44:29 -0400

    reviews almost complete

--- js/data.js
+++ js/data.js
@@ -1,39 +1,39 @@
 
 window.reviewData = [
     {
+        location: 'Guildwood Park',
+        name:'Sarah M.',
+        date: '2023/04/10',
+        rating: '3',
+        comment: 'Its kinda boring and there is not much to do here.',
+    },
+    {
         location: 'Grey Abbey Park',
         name:'Naushin R.',
-        date: '2023-08-09',
-        rating: '5/5',
+        date: '2023/01/14',
+        rating: '5',
         comment: 'Its so peaceful and serene, i love listening to the birds',
     },
     {
         location: 'Midori Ramen',
         name:'Jermaine R.',
-        date: '2023-08-09',
-        rating: '5/5',
+        date: '2023/07/19',
+        rating: '4',
         comment: "Theyre garlic ramen is amazing, I'll be sure to come back with my whole family."
     },
     {
-        location: 'Guildwood Park',
-        name:'Sarah M.',
-        date: '2023-08-09',
-        rating: '5/5',
-        comment: 'Its so peaceful and serene, I love listening to the birds',
-    },
-    {
         location: 'Arctic Bites',
-        name:'Naushin',
-        date: '2023-08-09',
-        rating: '5/5',
-        comment: 'Its so peaceful and serene, I love listening to the birds',
+        name:'Michael J.',
+        date: '2023/08/09',
+        rating: '2',
+        comment: 'Ice cream is too sweet and line was wayy too long',
     },
     {
         location: 'Saving Mondays',
-        name:'Naushin',
-        date: '2023-08-09',
-        rating: '5/5',
-        comment: "Staff as so kind and their cookies are amazing!! Love coming here to work! It's such a cozy and cute space",
+        name:'Giuliana S.',
+        date: '2023/08/10',
+        rating: '4',
+        comment: "Staff as so kind and their cookies are amazing!! Love coming here to work! It's such a cozy and cute space.",
     }
 ];
 
@@ -47,7 +47,7 @@ function reviewCards() {
   
       const rating = document.createElement("p");
       rating.classList.add("card-rating");
-      rating.textContent = review.rating;
+      rating.textContent = starRating(review.rating);
   
       const reviewText = document.createElement("p");
       reviewText.textContent = review.comment;
@@ -58,11 +58,12 @@ function reviewCards() {
       date.classList.add("card-date");
   
       const name = document.createElement("p");
-      name.textContent = `(${review.name})`;
+      name.textContent = `${review.name}`;
       name.classList.add("card-name");
 
       const location = document.createElement("h3");
       location.textContent = review.location;
+      location.classList.add("card-location");
 
       card.appendChild(location);
       card.appendChild(rating);
@@ -73,5 +74,35 @@ function reviewCards() {
       reviewCont.appendChild(card);
     });
   }
+
+  function starRating(rating) {
+    const totalStar = 5;
+    const fullStar = "★".repeat(rating);
+    const emptyStar = "☆".repeat(totalStar - rating);
+    return fullStar + emptyStar;
+  }
+
+  function addReview() {
+    const name = document.getElementById("name").value;
+    const date = document.getElementById("date").value;
+    const rating = parseInt(document.getElementById("rating").value);
+    const reviewText = document.getElementById("reviewText").value;
+  
+    // Validation
+    if (name == "" || date == "" || rating == "" || reviewText == "") {
+      alert("Please enter your information and try again");
+      return false;
+    }
+  
+    reviewData.push({
+      name: name,
+      date: date,
+      rating: rating,
+      text: reviewText
+    });
+  
+    reviewCards();
+  }
+  
 
   window.onload = reviewCards();
